154e4d04a2cfc63e85041382cd67308e112c4cee
lafnowcrylater <Ponake24565@gmail.com>
2026-02-25 23:30:31 +0700

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import './layout.css';
+	import documentIcon from '$lib/assets/document-gray.png';
+
+	let { children } = $props();
+</script>
+
+<svelte:head><link rel="icon" href={documentIcon} /></svelte:head>
+{@render children()}
